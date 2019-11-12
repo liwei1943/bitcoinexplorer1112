26bef4965ca5d40768245304eacf7b1668e6415a
liwei1943 <1478223810@qq.com>
2019-11-12 10:47:05 +0800

    init project

---
+++ bitcoinexplorer1112background/src/main/java/io/lw/bitcoinexplorer1112background/Bitcoinexplorer1112backgroundApplication.java
@@ -0,0 +1,13 @@
+package io.lw.bitcoinexplorer1112background;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bitcoinexplorer1112backgroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bitcoinexplorer1112backgroundApplication.class, args);
+    }
+
+}
---
+++ bitcoinexplorer1112background/src/test/java/io/lw/bitcoinexplorer1112background/Bitcoinexplorer1112backgroundApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lw.bitcoinexplorer1112background;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bitcoinexplorer1112backgroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

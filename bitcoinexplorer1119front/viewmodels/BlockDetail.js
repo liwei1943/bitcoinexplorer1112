cce0180155fcdb29e2753602f94aa8acdb68ad82
liwei1943 <1478223810@qq.com>
2019-11-20 20:51:22 +0800

    add block txDetail list page

--- bitcoinexplorer1119front/viewmodels/BlockDetail.js
+++ bitcoinexplorer1119front/viewmodels/BlockDetail.js
@@ -4,6 +4,10 @@ var app = new Vue({
 
         blockhash: "",
         BlcokDetail: {},
+        page: 1,
+        total: 0,
+        pageSize: 10,
+        txPageinfo: "",
     },
     methods: {
         getBlockByBlockHash() {
@@ -14,11 +18,33 @@ var app = new Vue({
                 .catch(function (error) {
                     console.log(error);
                 });
+        },
+        getTransactionByBlockhashWithPage() {
+            axios.get("/transaction/getTransactionByBlockhashWithPage", { params: { blockhash: this.blockhash, page: this.page } }).then(function (response) {
+                console.log(response);
+                app.txPageinfo = response.data;
+                app.page = response.data.page;
+                app.total = response.data.total;
+                app.pageSize = response.data.pageSize;
+            })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        },
+        handleSizeChange(val) {
+            this.pageSize = val;
+            this.getTransactionByBlockhashWithPage(this.page, this.pageSize);
+        },
+        //当前页
+        handleCurrentChange(val) {
+            this.page = val;
+            this.getTransactionByBlockhashWithPage(this.page, this.pageSize);
         }
     },
     mounted() {
         var url = new URL(location.href);
         this.blockhash = url.searchParams.get("blockhash");
         this.getBlockByBlockHash();
+        this.getTransactionByBlockhashWithPage();
     }
 })
